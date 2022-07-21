8a5159bbd47a78d60a309976d43da025944273ae
Omri <omriluz1994@gmail.com>
2022-07-21 13:58:52 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors:true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['image.shutterstock.com', 'lh3.googleusercontent.com']
